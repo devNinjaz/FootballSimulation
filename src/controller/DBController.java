ba96d1b0844fe93d6b7245cadbb4a4690020449f
devNinjaz <nmicovic@outlook.com>
2016-01-30 18:23:06 +0100

    Some slight code tweaks and optimizations.

--- src/controller/DBController.java
+++ src/controller/DBController.java
@@ -26,38 +26,26 @@ public class DBController
     public ResultSet sendQuery(String query)
     {
         try {
-            //STEP 2: Register JDBC driver
+            // STEP 2: Register JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
 
-            //STEP 3: Open a connection
+            // STEP 3: Open a connection
             System.out.println("Connecting to database...");
             connection = DriverManager.getConnection(DB_URL, USER, PASS);
 
-            //STEP 4: Execute a query
+            // STEP 4: Execute a query
             System.out.println("Creating statement...");
             statement = connection.createStatement();
 
             resultSet = statement.executeQuery(query);
             return resultSet;
-//            //STEP 5: Extract data from result set
-//            while (resultSet.next()) {
-//                //Retrieve by column name
-//                int id = resultSet.getInt("player_id");
-//                String first = resultSet.getString("name");
-//                String last = resultSet.getString("surname");
-//
-//                //Display values
-//                System.out.print("ID: " + id);
-//                System.out.print(", First: " + first);
-//                System.out.println(", Last: " + last);
-//            }
 
 
         } catch (SQLException se) {
-            //Handle errors for JDBC
+            // Handle errors for JDBC
             se.printStackTrace();
         } catch (Exception e) {
-            //Handle errors for Class.forName
+            // Handle errors for Class.forName
             e.printStackTrace();
         }
         return null;
@@ -71,9 +59,8 @@ public class DBController
             resultSet.close();
         } catch (SQLException e) {
             System.out.println("Failed closing DB connection!");
-//            e.printStackTrace();
         } finally {
-            //finally block used to close resources
+            // finally block used to close resources
             try {
                 if (statement != null)
                     statement.close();
@@ -84,7 +71,7 @@ public class DBController
                     connection.close();
             } catch (SQLException se) {
                 se.printStackTrace();
-            }//end finally try
+            } //end finally try
         }
     }
 }
