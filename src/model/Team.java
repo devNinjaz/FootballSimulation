b30cee354c09fab5bd5591519f36e95f46c04747
devNinjaz <nmicovic@outlook.com>
2016-01-31 00:09:55 +0100

    Minor bugfixes.

--- src/model/Team.java
+++ src/model/Team.java
@@ -125,6 +125,36 @@ public class Team
         }
     }
 
+    public Player getShooter()
+    {
+        Vector<Player> homies = new Vector<Player>(players);
+
+        // Sorting players by their best shooting stats
+        Collections.sort(homies, (o1, o2) ->
+                (o2.getStats().get("attacking_prowess") + o2.getStats().get("finishing") + o2.getStats().get("kicking_power"))
+                - (o1.getStats().get("attacking_prowess") + o1.getStats().get("finishing") + o1.getStats().get("kicking_power"))
+        );
+
+        RandomUtil rand = new RandomUtil();
+        float val = rand.runif();
+        if (val <= 0.60f) {
+            // 6666660% chances that best two shooters will take a shot
+            val = rand.runif();
+            if (val <= 0.55f)
+                return homies.get(0);
+            else
+                return homies.get(1);
+        } else {
+            val = rand.runif();
+            if (val <= 0.7f) {
+                // 3 4
+                return homies.get(rand.getFromInterval(2, 3));
+            } else {
+                return homies.get(rand.getFromInterval(4, 10));
+            }
+        }
+    }
+
     public Player getMidfielder()
     {
         Vector<Player> homies = new Vector<>();
@@ -204,4 +234,145 @@ public class Team
     {
         return teamName;
     }
+
+    public Player getBestPasser()
+    {
+        Vector<Player> homies = new Vector<Player>(players);
+        Collections.sort(homies, (o1, o2) ->
+                (o2.getStats().get("low_pass") + o2.getStats().get("lofted_pass"))
+                        - (o1.getStats().get("low_pass") + o1.getStats().get("lofted_pass"))
+        );
+        return homies.get(0);
+    }
+
+    public Player getPasser()
+    {
+        // In football, midfielders are the ones that have the most of the ball,
+        //  after them, defenders, and then attackers.
+        // In this simulation, we assume that around 40% of the passing is done by
+        // midfielders (dm, cm, am), 30% by defenders (cb, rb, lb), 20% by wingers,
+        // and 10% by strikers.
+        float probMidfield = 0.4f;
+        float probDefense = 0.3f;
+        float probWings = 0.2f;
+        float probStrikers = 0.1f;          // not used, just for illustration
+
+        RandomUtil rand = new RandomUtil();
+        float dragon = rand.runif();
+        Vector<Player> homies;
+        if (dragon <= probMidfield) {
+            // we choose among midfielders
+            homies = getMidfilders();
+        } else if (dragon <= probMidfield + probDefense) {
+            // we choose defense
+            homies = getDefenders();
+        } else if (dragon <= probMidfield + probDefense + probWings) {
+            // we choose wings
+            homies = getWingers();
+        } else {
+            // we choose strikers
+            homies = getStrikers();
+        }
+
+        // Now we pick a player to play a pass from our chose players
+        // 60% that it's the best passer among them, 40% that is someone else
+
+        // sorting by passing
+        Collections.sort(homies, (o1, o2) ->
+                (o2.getStats().get("low_pass") + o2.getStats().get("lofted_pass"))
+                        - (o1.getStats().get("low_pass") + o1.getStats().get("lofted_pass"))
+        );
+        dragon = rand.runif();
+        if (dragon <= 0.6f) {
+            return homies.get(0);
+        } else {
+            if (homies.size() == 1) return homies.get(0);
+            else return homies.get(rand.getFromInterval(1, homies.size()-1));
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    // Returns team players by position
+    // --------------------------------------------------------------------------------------------------------
+    public Vector<Player> getWingers()
+    {
+       Vector<Player> homies = new Vector<>();
+        for (Player homie: players)
+            if (homie.getPosition().equals("lw") || homie.getPosition().equals("rw"))
+                homies.add(homie);
+        return homies;
+    }
+
+    public Vector<Player> getDefenders()
+    {
+        Vector<Player> homies = new Vector<>();
+        for (Player homie: players)
+            if (homie.getPosition().equals("cb") || homie.getPosition().equals("rb") || homie.getPosition().equals("lb"))
+                homies.add(homie);
+        return homies;
+    }
+
+    public Vector<Player> getMidfilders()
+    {
+        Vector<Player> homies = new Vector<>();
+        for (Player homie: players)
+            if (homie.getPosition().equals("dm") || homie.getPosition().equals("cm") || homie.getPosition().equals("am"))
+                homies.add(homie);
+        return homies;
+    }
+
+    public Vector<Player> getStrikers()
+    {
+        Vector<Player> homies = new Vector<>();
+        for (Player homie: players)
+            if (homie.getPosition().equals("st"))
+                homies.add(homie);
+        return homies;
+    }
+    // --------------------------------------------------------------------------------------------------------
+
+    public Player getInterceptor()
+    {
+        // Returns a player likely to try to intercept a pass
+        // Players with most interceptions are mostly midfielders and defenders
+        //  As such, we shall distribute: 40% midfielders, 35% defenders, 15% wingers, 5% strikers
+        float probMidfield = 0.4f;
+        float probDefense = 0.35f;
+        float probWings = 0.15f;
+        float probStrikers = 0.05f;          // not used, just for illustration
+
+        RandomUtil rand = new RandomUtil();
+        float dragon = rand.runif();
+        Vector<Player> homies;
+        if (dragon <= probMidfield) {
+            // we choose among midfielders
+            homies = getMidfilders();
+        } else if (dragon <= probMidfield + probDefense) {
+            // we choose defense
+            homies = getDefenders();
+        } else if (dragon <= probMidfield + probDefense + probWings) {
+            // we choose wings
+            homies = getWingers();
+        } else {
+            // we choose strikers
+            homies = getStrikers();
+        }
+
+        // Now we pick a player to play a pass from our chose players
+        // 60% that it's the best interceptor among them, 40% that is someone else
+
+        // sorting by interception skill
+        Collections.sort(homies, (o1, o2) ->
+                (o2.getStats().get("defensive_prowess") + o2.getStats().get("ball_winning"))
+                        - (o1.getStats().get("defensive_prowess") + o1.getStats().get("ball_winning"))
+        );
+        dragon = rand.runif();
+        if (dragon <= 0.6f) {
+            return homies.get(0);
+        } else {
+            if (homies.size() == 1) return homies.get(0);
+            else return homies.get(rand.getFromInterval(1, homies.size()-1));
+        }
+    }
+
 }
